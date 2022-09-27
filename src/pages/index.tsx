d5df4639f7d793f542eb32dd9eb839d3e6617eba
Jaron Heard <jaronheard@gmail.com>
2022-09-26 23:04:36 -0400

    set up login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 const Home = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <main>Loading...</main>;
+  }
+
   return (
     <main>
       <h1>Home</h1>
+      {session ? (
+        <div>
+          <p>Signed in as {session.user?.email}</p>
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign in with Google</button>
+      )}
     </main>
-  )
-}
+  );
+};
 
 export default Home;
