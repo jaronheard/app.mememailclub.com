5a505125684349c8b68914478407b5bfb4878466
Jaron Heard <jaronheard@gmail.com>
2022-10-03 14:39:46 -0700

    update file upload styling

--- src/components/SimpleFileUpload.tsx
+++ src/components/SimpleFileUpload.tsx
@@ -34,38 +34,52 @@ const FileUpload = ({
     });
 
   return (
-    <div>
-      {url && (
-        <a href={url} target="_blank" rel="noreferrer">
-          File
-        </a>
-      )}
-      <input
-        {...register(id, { required })}
-        aria-describedby="file_input_help"
-        id={`${name}-file-upload`}
-        type="file"
-        onChange={(e) => {
-          if (e.target?.files?.[0]) {
-            setStatus("uploading...");
-            uploadFile(e.target.files[0]).then((url) => {
-              setStatus("uploaded");
-              setUrl(url);
-            });
-          }
-        }}
-      />
+    <div className="flex flex-col gap-2">
+      <label
+        htmlFor={`${id}-file-upload`}
+        className="block text-sm font-medium capitalize text-gray-700"
+      >
+        {id}
+      </label>
+      <div className="flex gap-3">
+        <label
+          htmlFor={`${id}-file-upload`}
+          className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:bg-indigo-700"
+        >
+          {status === "uploading" ? "Uploading..." : "Upload File"}
+          <input
+            className="sr-only"
+            {...register(id, { required })}
+            aria-describedby="file_input_help"
+            id={`${id}-file-upload`}
+            type="file"
+            onChange={(e) => {
+              if (e.target?.files?.[0]) {
+                setStatus("uploading");
+                uploadFile(e.target.files[0]).then((url) => {
+                  setStatus("uploaded");
+                  setUrl(url);
+                });
+              }
+            }}
+          ></input>
+        </label>
+        {url && status !== "uploading" && (
+          <a
+            className="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-3 py-2 text-sm font-medium leading-4 text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            View File
+          </a>
+        )}
+      </div>
       <p
         className="mt-1 text-sm text-gray-500 dark:text-gray-300"
         id="file_input_help"
       >
         {label}
-      </p>
-      <p
-        className="mt-1 text-sm text-gray-500 dark:text-gray-300"
-        id="file_input_help"
-      >
-        {status === "Uploading..."}
       </p>
     </div>
   );
