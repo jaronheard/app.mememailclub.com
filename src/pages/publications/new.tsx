daeafd024ef8c5646a654e5ae9357f110a60f2ea
Jaron Heard <jaronheard@gmail.com>
2022-10-03 19:35:09 -0700

    fix links

--- src/pages/publications/new.tsx
+++ src/pages/publications/new.tsx
@@ -181,7 +181,7 @@ const New = () => {
                       status: "DRAFT",
                     });
 
-                    router.push("/");
+                    router.push("/publications");
                   })}
                   className="ml-3 inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
@@ -198,7 +198,7 @@ const New = () => {
                       status: "PUBLISHED",
                     });
 
-                    router.push("/");
+                    router.push("/publications");
                   })}
                   className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
