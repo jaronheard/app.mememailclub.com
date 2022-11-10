86bfd7e654ca9a90bba9491a42581d87d31a7dd4
Jaron Heard <jaronheard@gmail.com>
2022-11-10 15:19:10 -0800

    update link

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -16,14 +16,14 @@ const Breadcrumbs = ({ pages }: BreadcrumbsProps) => {
       <ol role="list" className="flex items-center space-x-4">
         <li>
           <div>
-            <Link href="/publications">
-              <a className="text-gray-400 hover:text-gray-500">
-                <PencilSquareIcon
-                  className="h-5 w-5 flex-shrink-0"
-                  aria-hidden="true"
-                />
-                <span className="sr-only">Home</span>
-              </a>
+            <Link href="/publications" className="text-gray-400 hover:text-gray-500">
+
+              <PencilSquareIcon
+                className="h-5 w-5 flex-shrink-0"
+                aria-hidden="true"
+              />
+              <span className="sr-only">Home</span>
+
             </Link>
           </div>
         </li>
@@ -34,14 +34,13 @@ const Breadcrumbs = ({ pages }: BreadcrumbsProps) => {
                 className="h-5 w-5 flex-shrink-0 text-gray-400"
                 aria-hidden="true"
               />
-              <Link href={page.href}>
-                <a
-                  href={page.href}
-                  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-                  aria-current={page.current ? "page" : undefined}
-                >
-                  {page.name}
-                </a>
+              <Link
+                href={page.href}
+                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                aria-current={page.current ? "page" : undefined}>
+
+                {page.name}
+
               </Link>
             </div>
           </li>
