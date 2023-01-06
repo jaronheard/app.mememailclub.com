16d447ec41677a2a5283f321ca341d7fe7641532
Jaron Heard <jaronheard@gmail.com>
2023-01-05 21:34:57 -0800

    update favicon from realfavicongenerator.net

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,33 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="bg-gray-100 h-full">
+    <Html className="h-full bg-gray-100">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body className="h-full">
         <Main />
