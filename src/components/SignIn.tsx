7329879ca81cb309eef2f55a0c4b2c2cb09dbe67
Jaron Heard <jaronheard@gmail.com>
2023-03-09 22:53:41 -0800

    login screen design tweaks

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -4,9 +4,9 @@ import Img from "./Img";
 export default function SignIn() {
   return (
     <>
-      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white py-8 px-4 shadow-lg sm:rounded-lg sm:px-10">
+      <div className="flex min-h-full flex-col justify-center sm:p-6 lg:p-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="py-8 px-4 sm:px-10">
             <svg
               width="26"
               height="37"
