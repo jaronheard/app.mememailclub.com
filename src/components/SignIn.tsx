c6660fda8c04add484f4398e6c882f73b65f2980
Jaron Heard <jaronheard@gmail.com>
2023-03-09 22:38:23 -0800

    update signin language

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -14,7 +14,7 @@ export default function SignIn() {
             height={118}
           />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">
-            Post-postcard
+            PostPostcard
           </h2>
         </div>
 
@@ -30,8 +30,7 @@ export default function SignIn() {
                 <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
               </svg>
               <p className="relative">
-                To create is to be human. To share is to be human. To share your
-                creations is to be a Post-postcard member.
+                To create or send postcards, use your Google Account to login.
               </p>
             </div>
             <p className="mt-2 text-sm font-medium text-gray-700">
