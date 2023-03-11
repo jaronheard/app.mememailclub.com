a2cfbfe75b35a93f0cad3bbbee1653eebd46ac58
Jaron Heard <jaronheard@gmail.com>
2023-03-11 12:50:55 -0800

    remove unused code

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,14 +1,6 @@
 export const CLOUDINARY_UPLOAD_API_URL =
   "https://api.cloudinary.com/v1_1/jaronheard/auto/upload";
 
-// docs: https://cloudinary.com/documentation/fetch_remote_images#fetch_and_deliver_remote_files
-export const CLOUDINARY_IMAGE_FETCH_URL =
-  "https://res.cloudinary.com/jaronheard/image/fetch/";
-
-export const cacheImageUrl = (url: string) => {
-  return `${CLOUDINARY_IMAGE_FETCH_URL}${url}`;
-};
-
 export async function uploadFile(file: File) {
   const formData = new FormData();
   formData.append("file", file);
