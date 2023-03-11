1e895d7fe2f68f5dbffd4d1388229409ef4de1b4
Jaron Heard <jaronheard@gmail.com>
2023-03-11 12:48:09 -0800

    add image caching

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,4 +1,13 @@
-const url = "https://api.cloudinary.com/v1_1/jaronheard/auto/upload";
+export const CLOUDINARY_UPLOAD_API_URL =
+  "https://api.cloudinary.com/v1_1/jaronheard/auto/upload";
+
+// docs: https://cloudinary.com/documentation/fetch_remote_images#fetch_and_deliver_remote_files
+export const CLOUDINARY_IMAGE_FETCH_URL =
+  "https://res.cloudinary.com/jaronheard/image/fetch/";
+
+export const cacheImageUrl = (url: string) => {
+  return `${CLOUDINARY_IMAGE_FETCH_URL}${url}`;
+};
 
 export async function uploadFile(file: File) {
   const formData = new FormData();
@@ -7,7 +16,7 @@ export async function uploadFile(file: File) {
 
   // try to upload file
   try {
-    const response = await fetch(url, {
+    const response = await fetch(CLOUDINARY_UPLOAD_API_URL, {
       method: "POST",
       body: formData,
     });
