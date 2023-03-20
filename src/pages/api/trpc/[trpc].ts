b1b8f12b9ce0878b1fcd59aaf7c46ec1367fd3ba
Jaron Heard <jaronheard@gmail.com>
2023-03-20 13:15:04 -0700

    add caching

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -6,6 +6,27 @@ import { createContext } from "../../../server/router/context";
 export default createNextApiHandler({
   router: appRouter,
   createContext,
+  responseMeta: ({ ctx, paths, type, errors }) => {
+    const cachedPaths =
+      paths &&
+      paths.every(
+        (path) => path.includes("items.") || path.includes("publications.")
+      );
+    // checking that no procedures errored
+    const allOk = errors.length === 0;
+    // checking we're doing a query request
+    const isQuery = type === "query";
+    if (ctx && cachedPaths && allOk && isQuery) {
+      // cache request for 1 day + revalidate once every second
+      const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+      return {
+        headers: {
+          "cache-control": `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+        },
+      };
+    }
+    return {};
+  },
   onError: ({ path, error, type, input }) => {
     console.error("trpc error", { path, error, type, input });
   },
